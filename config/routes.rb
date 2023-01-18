506778fb12614ed9685061a63aab8cb7372f5c3f
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2023-01-18 14:18:52 +0300

    this commit adds custom routes to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :alerts
   resources :reveals
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:update]
 
+  get "/me", to: "users#show"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/users", to: "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
