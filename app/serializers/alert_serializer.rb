bda1c17179185d739ddea6f310b7ded3976b37ff
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2023-01-21 01:44:22 +0300

    this commit removes some authorization to some routes

--- app/serializers/alert_serializer.rb
+++ app/serializers/alert_serializer.rb
@@ -1,3 +1,5 @@
 class AlertSerializer < ActiveModel::Serializer
   attributes :id, :title, :message, :image, :status, :lat, :lng, :user_id
+
+  belongs_to :user
 end
--- app/serializers/reveal_serializer.rb
+++ app/serializers/reveal_serializer.rb
@@ -1,3 +1,5 @@
 class RevealSerializer < ActiveModel::Serializer
   attributes :id, :report_title, :report_message, :report_image, :report_status
+
+  belongs_to :user
 end
